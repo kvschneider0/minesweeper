831edf3d64db6cbd3d41dfa92d1ab0a299bad2c9
Kyle <kylesch115@gmail.com>
2023-06-30 19:51:08 -0600

    setup app with TS

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import Game from "./components/Game/Game";
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>Minesweeper</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Game/Game.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Game: FC = () => {
+  return (
+    <div>
+      <h2>game page</h2>
+    </div>
+  );
+}
+
+export default Game;
